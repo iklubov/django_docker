2ca7ba229f5dd0bb2cfe6db82885b586dc09ba4c
GnomReal <vgnommy@gmail.com>
2022-04-11 01:17:05 +0300

    simple redirect added

--- redirect_urls/views.py
+++ redirect_urls/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def redirect_view():
+    response = redirect('/redirect-success/')
+    return response
