bfbc9220dcc076c7301750847ab5b8bb78d81962
GnomReal <vgnommy@gmail.com>
2022-04-11 01:26:47 +0300

    error in the function args

--- redirect_urls/views.py
+++ redirect_urls/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import redirect
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def redirect_view():
+def redirect_view(request):
     response = redirect('/redirect-success/')
     return response
