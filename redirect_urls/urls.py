68c11bf525bb9d601ad199d67dacbe06f30dbc71
GnomReal <vgnommy@gmail.com>
2022-04-12 10:59:00 +0300

    circle redirect support

--- redirect_urls/urls.py
+++ redirect_urls/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('redirect/', views.redirect_view)
+    path('redirect/', views.redirect_view),
+    re_path(r'redirect_circle(_\d+)?\/', views.cirle_redirect_view)
 ]
--- redirect_urls/views.py
+++ redirect_urls/views.py
@@ -1,10 +1,18 @@
+import itertools
+
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+circle_iterator = itertools.cycle(range(5))
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def redirect_view(request):
     response = redirect('/redirect-success/')
+    return response
+
+def cirle_redirect_view(request):
+    redirect_index =  next(circle_iterator)
+    response = redirect('/redirect_circle_{}/'.format(redirect_index))
     return response
